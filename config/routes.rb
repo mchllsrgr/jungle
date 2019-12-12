4fec2623324b9b006146f6e0d8ed322c8a73a9ed
Michelle Siregar <michellesiregar@gmail.com>
2019-12-12 21:32:04 +0000

    update schema and route for users

--- config/routes.rb
+++ config/routes.rb
@@ -19,6 +19,9 @@ Rails.application.routes.draw do
 
   resource :about, only: [:show]
 
+  resource :users, only: [:new, :create]
+  get '/signup', to: 'users#new'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
