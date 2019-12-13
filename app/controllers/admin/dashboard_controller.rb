e5b3b2f9461d7d6adf68a9d4244aa521f3f5fffd
Michelle Siregar <michellesiregar@gmail.com>
2019-12-13 03:29:56 +0000

    add admin authentication

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,4 +1,6 @@
 class Admin::DashboardController < ApplicationController
+  http_basic_authenticate_with name: ENV['username'], password: ENV['password']
+  
   def show
     @prod_total = Product.all.count
     @cat_total = Category.all.count
