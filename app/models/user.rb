178a076863d36d121cc492b5be9b9923ec93127a
Michelle Siregar <michellesiregar@gmail.com>
2019-12-17 01:21:01 +0000

    create email/pw authentication in user model, removed auth from sessions controller

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,14 @@ class User < ActiveRecord::Base
   validates :email, presence: true, uniqueness: { case_sensitive: false }
   validates :password, presence: true, length: { minimum: 5 }
   validates :password_confirmation, presence: true, length: { minimum: 5 }
+
+  def self.authenticate_with_credentials(email, password)
+    @user = User.find_by_email(email)
+    if @user.authenticate(password)
+      @user
+    else
+     nil
+    end
+  end
+  
 end
